89211566768deb2627e31cdb5e99251f473a1c0c
Sairam Rakshith Bhyravabhotla <bsairamr@tank.soic.indiana.edu>
2017-02-19 17:58:11 -0500

    insert

--- data.py
+++ data.py
@@ -1,10 +1,11 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Table
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+#from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-
+from sqlalchemy.orm import sessionmaker, relationship, scoped_session
+from sqlalchemy import (update, insert, and_)
 Base = declarative_base()
 
 class Posts(Base):
@@ -31,6 +32,22 @@ class Users(Base):
 	DownVotes = Column(Integer)
 	Age = Column(Integer)		
 
+	def __init__(self, Id, Reputation, Location, Views, UpVotes, DownVotes, Age):
+		self.Id= Id
+		self.Reputation = Reputation
+		self.Location = Location
+		self.Views = Views
+		self.UpVotes = UpVotes
+		self.DownVotes = DownVotes
+		self.Age = Age
+	
+	def __str__(self):
+		return unicode(self).encode('utf-8')
+
 engine = create_engine('sqlite:///stackoverflow.db') 
+Base = declarative_base()
 Base.metadata.create_all(engine)
 Session = scoped_session(sessionmaker(bind=engine))
+
+Session.execute(insert(Users).values(Id = 123, Reputation = 21, Location= "test", Views= 2, UpVotes = 2, DownVotes = 3, Age = 22))
+
