be5431896712ec32e0c64dc8bfb84eea996a37dd
Sairam Rakshith Bhyravabhotla <bsairamr@tank.soic.indiana.edu>
2017-03-23 17:04:28 -0400

    spacesz to tabs

--- queries.py
+++ queries.py
@@ -8,9 +8,9 @@ from data import Location, Session, Users
 session = Session()
 
 def insert_users(Id, Reputation, Location, Views, UpVotes, DownVotes, Age):
-    session.execute(insert(Users).values(Id = Id, Reputation = Reputation, Location= Location, Views= Views, UpVotes = UpVotes, DownVotes = DownVotes, Age = Age))
-    session.commit()
-    return "User information successfully inserted"
+	session.execute(insert(Users).values(Id = Id, Reputation = Reputation, Location= Location, Views= Views, UpVotes = UpVotes, DownVotes = DownVotes, Age = Age))
+	session.commit()
+	return "User information successfully inserted"
 
 def get_unique_locations():
 	locations=[]
@@ -33,27 +33,27 @@ def insert_locations():
 	return "success"
 
 def parse_xml_file(xml_file):
-    list_of_fields = []
-    user_object = {}
-    field_counter = 0
-    with open(xml_file,'r') as f:
-        for x in f:
-            x = x.strip()
-            if x.startswith("<row"):
-                node = etree.fromstring(x)
-                for key in node.keys():
-                   # if (field_counter == 0):
-                   #    list_of_fields.append(key)
-                    user_object[key] = node.attrib[key]    
-                #print " PRINTING THE RECORDS NOW "
-                users = data.Users(user_object.get('Id'),user_object.get('Reputation'),user_object.get('Location'),user_object.get('Views'),user_object.get('UpVotes'),user_object.get('DownVotes'),user_object.get('Age'))
-                session = data.Session()
-                session.add(users)
-                session.commit()
-				#print user_object    
-                field_counter += 1
-                print field_counter
-                user_object = {}
+	list_of_fields = []
+	user_object = {}
+	field_counter = 0
+	with open(xml_file,'r') as f:
+		for x in f:
+			x = x.strip()
+			if x.startswith("<row"):
+				node = etree.fromstring(x)
+				for key in node.keys():
+				   # if (field_counter == 0):
+				   #	list_of_fields.append(key)
+					user_object[key] = node.attrib[key]    
+				#print " PRINTING THE RECORDS NOW "
+				users = data.Users(user_object.get('Id'),user_object.get('Reputation'),user_object.get('Location'),user_object.get('Views'),user_object.get('UpVotes'),user_object.get('DownVotes'),user_object.get('Age'))
+				session = data.Session()
+				session.add(users)
+				session.commit()
+				#print user_object	  
+				field_counter += 1
+				print field_counter
+				user_object = {}
 
 if __name__=="__main__":
 	print insert_locations()	
