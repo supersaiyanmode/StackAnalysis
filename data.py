f3bee70b3f8dfec57e9246b1a5ff6c7c0c657099
rangira <rangira@umail.iu.edu>
2017-03-23 01:48:43 -0400

    Code clean up

--- data.py
+++ data.py
@@ -1,87 +1,89 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime,  UniqueConstraint, Table
+
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import UniqueConstraint, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,relationship,scoped_session
+
 from dateutil import parser
 
 
 Base = declarative_base()
 
+
 poststags_table  = Table('poststags',Base.metadata,
-                    Column('PostsId', Integer(), ForeignKey('posts.id')),
-                    Column('TagsId', Integer(), ForeignKey('tags.id'))
-                    )
+							Column('PostsId', Integer(), ForeignKey('questions.id')),
+							Column('TagsId', Integer(), ForeignKey('tags.id')))
+
 
 class Posts(Base):
-    __tablename__ = 'posts'
+	__tablename__ = 'posts'
 
-    id = Column(Integer, primary_key=True)
-    PostTypeId = Column(Integer)
-    AcceptedAnswerId = Column(Integer)
-    ParentId = Column(Integer)
-    CreationDate = Column(DateTime)
-    Score = Column(Integer)
-    OwnerUserId = Column(Integer)
-    LastActivityDate = Column(DateTime)
-    Tags = Column(String)
-    AnswerCount = Column(Integer)
-    
-    def __init__(self,id,PostTypeId,AcceptedAnswerId,ParentId,\
-		CreationDate,Score,OwnerUserId,LastActivityDate,Tags,AnswerCount):
-        self.id = id
-        print self.AcceptedAnswerId," ",AcceptedAnswerId," ",PostTypeId," ",self.PostTypeId
-        self.PostTypeId = PostTypeId
-        self.AcceptedAnswerId = AcceptedAnswerId
-        self.ParentId = ParentId
-        self.CreationDate = parser.parse(CreationDate)
-        self.Score = Score
-        self.OwnerUserId = OwnerUserId
-        self.LastActivityDate = parser.parse(LastActivityDate)
-        self.Tags = Tags
-        self.AnswerCount = AnswerCount
-        
+	id = Column(Integer, primary_key=True)
+	PostTypeId = Column(Integer)
+	AcceptedAnswerId = Column(Integer)
+	ParentId = Column(Integer)
+	CreationDate = Column(DateTime)
+	Score = Column(Integer)
+	OwnerUserId = Column(Integer, ForeignKey('users.Id'))
+	LastActivityDate = Column(DateTime)
+	Tags = Column(String)
+	AnswerCount = Column(Integer)
+
+
+class Questions(Base):
+	__tablename__ = 'questions'
+
+	id = Column(Integer, primary_key=True)
+	PostTypeId = Column(Integer)
+	AcceptedAnswerId = Column(Integer)
+	CreationDate = Column(DateTime)
+	Score = Column(Integer)
+	OwnerUserId = Column(Integer, ForeignKey('users.Id'))
+	LastActivityDate = Column(DateTime)
+	Tags = Column(String)
+	AnswerCount = Column(Integer)
+
+
+class Answers(Base):
+	__tablename__ = 'answers'
+
+	id = Column(Integer, primary_key=True)
+	PostTypeId = Column(Integer)
+	ParentId = Column(Integer)
+	CreationDate = Column(DateTime)
+	Score = Column(Integer)
+	OwnerUserId = Column(Integer, ForeignKey('users.Id'))
+	LastActivityDate = Column(DateTime)
+
+
 class Tags(Base):
-    __tablename__ = 'tags'
+	__tablename__ = 'tags'
 
-    id = Column(Integer, primary_key=True)
-    TagName = Column(String,index = True, unique = True)
-    posts = relationship("Posts", secondary = poststags_table)
+	id = Column(Integer, primary_key=True)
+	TagName = Column(String,index = True, unique = True)
+	posts = relationship("Questions", secondary = poststags_table)
 
-    __table_args__ = ( 
-                        UniqueConstraint("TagName"),
-                     )
+	__table_args__ = (UniqueConstraint("TagName"),)
 
-    def __init__(self, TagName):
-        #self.id = id
-        self.TagName = TagName
-
-
-class Location(Base):
-    __tablename__ = 'locations'
-
-    id = Column(Integer, primary_key = True)
-    LocationName = Column(String,ForeignKey('users.Location'))
-    CityName = Column(String)
-    StateName = Column(String)
-    CountryName = Column(String)
-    Offset = Column(Integer)  #offset will be in minutes
-    Left = Column(Float)
-    Right = Column(Float)
-    Top = Column(Float)
-    Bottom = Column(Float)
 
 class Users(Base):
 	__tablename__ = 'users'
+
 	Id = Column(Integer, primary_key= True)
 	Reputation = Column(Integer)
 	Location = Column(String)
 	Views = Column(Integer)
 	UpVotes = Column(Integer)
 	DownVotes = Column(Integer)
-	Age = Column(Integer)
+	Age = Column(Integer)	
+	posts = relationship ('Posts', backref = 'users', lazy = 'dynamic')
+	questions = relationship('Questions', backref = 'users')
+	answers = relationship('Answers', backref = 'users')
+
 
 engine = create_engine('sqlite:///posts.db') 
 Base.metadata.create_all(engine)
