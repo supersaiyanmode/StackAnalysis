dbc5afda9583ce9791a4f2add6faadd0b4322ef4
srivatsaniyer <srivatsan.iyer@healthfidelity.com>
2017-03-24 05:45:47 -0400

    Schema changes.

--- data.py
+++ data.py
@@ -18,60 +18,42 @@ poststags_table  = Table('poststags',Base.metadata,
 							Column('TagsId', Integer(), ForeignKey('tags.id')))
 
 
-class Posts(Base):
-	__tablename__ = 'posts'
-
-	id = Column(Integer, primary_key=True)
-	PostTypeId = Column(Integer)
-	AcceptedAnswerId = Column(Integer)
-	ParentId = Column(Integer)
-	CreationDate = Column(DateTime)
-	Score = Column(Integer)
-	OwnerUserId = Column(Integer, ForeignKey('users.Id'))
-	LastActivityDate = Column(DateTime)
-	Tags = Column(String)
-	AnswerCount = Column(Integer)
-
-
 class Questions(Base):
 	__tablename__ = 'questions'
 
-	Id = Column(Integer, primary_key=True)
-	PostTypeId = Column(Integer)
-	AcceptedAnswerId = Column(Integer)
-	CreationDate = Column(DateTime)
-	Score = Column(Integer)
-	OwnerUserId = Column(Integer, ForeignKey('users.Id'))
-	LastActivityDate = Column(DateTime)
-	AnswerCount = Column(Integer)
+	id = Column(Integer, primary_key=True)
+	accepted_answer = Column(Integer, ForeignKey('answers.id'))
+	score = Column(Integer)
+	author = Column(Integer, ForeignKey('users.Id'))
+	creation_date = Column(DateTime)
+	modified_date = Column(DateTime)
+	answer_count = Column(Integer) # TODO: To remove post-verification using joins
 
 
 class Answers(Base):
 	__tablename__ = 'answers'
 
-	Id = Column(Integer, primary_key=True)
-	PostTypeId = Column(Integer)
-	ParentId = Column(Integer)
-	CreationDate = Column(DateTime)
-	Score = Column(Integer)
-	OwnerUserId = Column(Integer, ForeignKey('users.Id'))
-	LastActivityDate = Column(DateTime)
+	id = Column(Integer, primary_key=True)
+	question = Column(Integer, ForeignKey('questions.id'))
+	score = Column(Integer)
+	author = Column(Integer, ForeignKey('users.Id'))
+	creation_date = Column(DateTime)
+	modified_date = Column(DateTime)
 
 
 class Tags(Base):
 	__tablename__ = 'tags'
 
 	id = Column(Integer, primary_key=True)
-	TagName = Column(String,index = True, unique = True)
+	name = Column(String,index = True, unique = True)
 	questions = relationship("Questions", secondary = poststags_table)
 
-	__table_args__ = (UniqueConstraint("TagName"),)
 
 class Location(Base):
 	__tablename__ = 'locations'
 
 	id = Column(Integer, primary_key = True, autoincrement= True)
-	location = Column(String(convert_unicode=True))
+	location = Column(String(convert_unicode=True), unique=True)
 	city = Column(String(convert_unicode=True))
 	state = Column(String(convert_unicode=True))
 	country = Column(String(convert_unicode=True))
@@ -84,21 +66,22 @@ class Location(Base):
 class Users(Base):
 	__tablename__ = 'users'
 
-	Id = Column(Integer, primary_key= True)
-	Reputation = Column(Integer)
-	Location = Column(String(convert_unicode=True))
-	Views = Column(Integer)
-	UpVotes = Column(Integer)
-	DownVotes = Column(Integer)
-	Age = Column(Integer)	
+	id = Column(Integer, primary_key= True)
+	reputation = Column(Integer)
+	location_id = Column(Integer, ForeignKey('locations.id'))
+	views = Column(Integer)
+	upvotes = Column(Integer)
+	downvotes = Column(Integer)
+	age = Column(Integer)
 	posts = relationship ('Posts', backref = 'users', lazy = 'dynamic')
 	questions = relationship('Questions', backref = 'users')
 	answers = relationship('Answers', backref = 'users')
 
 
 class ForeignKeysListener(PoolListener):
-    def connect(self, dbapi_con, con_record):
-        db_cursor = dbapi_con.execute('pragma foreign_keys=ON')
+	def connect(self, dbapi_con, con_record):
+		db_cursor = dbapi_con.execute('pragma foreign_keys=ON')
+
 
 listeners = [ForeignKeysListener()]
 engine = create_engine('sqlite:///' + sys.argv[1], listeners=listeners)
