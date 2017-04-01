ee9227df17e09624c8649662957b92c08c2874be
Rudrani Angira <rangira@marmot.soic.indiana.edu>
2017-04-01 00:46:04 -0400

    web service for locations

---
+++ app/controllers/locations.py
@@ -0,0 +1,39 @@
+from flask.views import MethodView
+from flask import Blueprint
+from flask import jsonify
+from sqlalchemy import distinct
+
+from app.models.data import Location, Session  
+
+location_handler = Blueprint('location_handler', __name__)
+
+class CountryController(MethodView):
+	def get(self):
+		countries = Session.query(distinct(Location.country))\
+					.filter(Location.country != None)\
+					.all()		
+		return jsonify(countries=countries)
+
+class StateController(MethodView):
+	def get(self, country):
+		states = Session.query(distinct(Location.state))\
+					.filter(Location.country == country)\
+					.filter(Location.state != None)\
+					.all()
+		return jsonify(states=states)
+
+class CityController(MethodView):
+	def get(self, country, state):
+		cities = Session.query(distinct(Location.city))\
+					.filter(Location.country == country)\
+					.filter(Location.state == state)\
+					.filter(Location.city != None)\
+					.all()
+		return jsonify(cities=cities)
+
+location_handler.add_url_rule('/countries/',
+				view_func=CountryController.as_view('countries'))
+location_handler.add_url_rule('/countries/<country>/states/', 
+				view_func=StateController.as_view('states'))
+location_handler.add_url_rule('/countries/<country>/states/<state>/cities/',
+				view_func=CityController.as_view('cities'))
