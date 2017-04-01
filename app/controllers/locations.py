ecefbe9558f9479cc505746e50a7f5778dfe5ae5
Srivatsan Iyer <srriyer@indiana.edu>
2017-04-01 01:43:45 -0400

    temporary commit.

--- app/controllers/locations.py
+++ app/controllers/locations.py
@@ -4,7 +4,7 @@ from flask.views import MethodView
 from flask import Blueprint
 from sqlalchemy import distinct
 
-from app.models.data import Location, Session  
+from app.models.data import Location, Session
 from app.utils import format_attrs
 
 location_handler = Blueprint('location_handler', __name__)
@@ -38,7 +38,7 @@ class CityController(MethodView):
 
 location_handler.add_url_rule('/countries/',
 				view_func=CountryController.as_view('countries'))
-location_handler.add_url_rule('/countries/<country>/states/', 
+location_handler.add_url_rule('/countries/<country>/states/',
 				view_func=StateController.as_view('states'))
 location_handler.add_url_rule('/countries/<country>/states/<state>/cities/',
 				view_func=CityController.as_view('cities'))
