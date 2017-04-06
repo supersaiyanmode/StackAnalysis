5d57083de35e0310f96629be1380c7492accf9e9
Sairam Rakshith Bhyravabhotla <bsairamr@indiana.edu>
2017-04-06 02:06:03 -0400

    changed default page num

--- app/utils/paginator.py
+++ app/utils/paginator.py
@@ -7,9 +7,9 @@ class Paginator:
 
 	def paginate(self, obj):
 		page_num = self.extract_page_num(request)
-		offset = (page_num-1) * self.page_size
+		offset = (page_num) * self.page_size
 		return obj.offset(offset).limit(self.page_size)
 
 	def extract_page_num(self, request):
-		return int(request.args.get('page', 1))
-		
+		return int(request.args.get('page', 0))
+
