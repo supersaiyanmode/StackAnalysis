f677a0e0bd955f7a29616eaef17ee30b1181f697
Srivatsan Iyer <srriyer@indiana.edu>
2017-04-07 22:59:26 -0400

    Temporary commit.

--- app/static/views/users/main.js
+++ app/static/views/users/main.js
@@ -1,49 +1,13 @@
-function loadUsers(page, filter, successFn, errorFn) {
-	var filt = "";
-	if (filter != null) {
-		filt = "&filter=" + encodeURIComponent(filter);
-	}
-	$.ajax({
-		type: "GET",
-		url: "/users/?page=" +page + filt
-	}).done(successFn).fail(errorFn);
-}
-
-function loadTablePage(num) {
-	var divSelector = "div.container-fluid div.table-row div.row";
-	loadUsers(num, null, function(data) {
-		loadTable(divSelector, data);
-		var params = {
-			total: Math.ceil(data.meta.rows / data.meta.page_size),
-			maxVisible: 10
-		};
-		loadPagination('#page-selection', params, loadPage);
-		loadFilterQuery('div.container-fluid .filter-query-table', data, updateUserFilter);
-	}, function(data) {
-		$(divSelector).html("Unable to load data.");
-	});
-}
-
-function loadPage(event, num) {
-	var divSelector = "div.container-fluid div.table-row div.row";
-	loadUsers(num, null, function(data) {
-		loadTable(divSelector, data);
-	}, function(data) {
-		$(divSelector).html("Unable to load data.");
-	});
-}
 
 function usersInit() {
-	loadTablePage(0);
-}
-
-function updateUserFilter(obj) {
-	var divSelector = "div.container-fluid div.table-row div.row";
-	loadUsers(0, JSON.stringify(obj), function(data) {
-		loadTable(divSelector, data);
-	}, function(data) {
-		$(divSelector).html("Unable to load data.");
-	});
+	var params = {
+		paginationSelector: 'div.container-fluid .pagination-bar',
+		queryFilterSelector: 'div.container-fluid .filter-query-table',
+		selector: "div.container-fluid div.table-row div.row",
+		url: "/users/"
+	}
+	tableObj = makeTable(params);
+	tableObj.load();
 }
 
 $(usersInit);
