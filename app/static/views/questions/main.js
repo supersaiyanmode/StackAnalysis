7d3d8a3463729f72e0592f7419df2d2cd13d8a42
Srivatsan Iyer <srriyer@indiana.edu>
2017-04-16 02:43:07 -0400

    Fixed markup and selectors to render tables/graphics.

--- app/static/views/questions/main.js
+++ app/static/views/questions/main.js
@@ -2,8 +2,8 @@ function questionsInit() {
 	var params = {
 		paginationSelector: 'div.container-fluid .pagination-bar',
 		queryFilterSelector: 'div.container-fluid .filter-query-table',
-		visualizationSelector: 'div.container-fluid div.visualization-row div.row',
-		selector: "div.container-fluid div.table-row div.row",
+		visualizationSelector: 'div.container-fluid div.visualization-row',
+		selector: "div.container-fluid div.table-row",
 		url: "/questions/"
 	}
 	tableObj = makeTable(params);
