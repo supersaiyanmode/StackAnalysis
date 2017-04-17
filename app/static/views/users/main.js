59068c9c63c0602c9a84a92fe95878299c3b8c4e
srivatsaniyer <srivatsan.iyer@healthfidelity.com>
2017-04-17 01:42:36 -0400

    Add selectors in JS.

--- app/static/views/users/main.js
+++ app/static/views/users/main.js
@@ -2,6 +2,7 @@ function usersInit() {
 	var params = {
 		paginationSelector: 'div.container-fluid .pagination-bar',
 		queryFilterSelector: 'div.container-fluid .filter-query-table',
+		orderBySelector: 'div.container-fluid .order-by-table',
 		visualizationSelector: 'div.container-fluid div.visualization-row',
 		orderBySelector: 'div.container-fluid .order-by-table',
 		selector: "div.container-fluid div.table-row",
--- app/static/views/view_answers_local_time/main.js
+++ app/static/views/view_answers_local_time/main.js
@@ -2,6 +2,7 @@ function viewAnswersLocalTimeInit() {
 	var params = {
 		paginationSelector: 'div.container-fluid .pagination-bar',
 		queryFilterSelector: 'div.container-fluid .filter-query-table',
+		orderBySelector: 'div.container-fluid .order-by-table',
 		visualizationSelector: 'div.container-fluid div.visualization-row',
 		timeChartSelector: 'div.container-fluid div.timechart-row',
 		orderBySelector: 'div.container-fluid .order-by-table',
