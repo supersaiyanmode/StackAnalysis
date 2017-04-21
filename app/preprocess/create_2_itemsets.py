fb72ba09b56d23c4803d4ed226774e222de965f8
Srivatsan Iyer <srriyer@indiana.edu>
2017-04-21 01:29:39 -0400

    Tiny script to retrieve intersection.

---
+++ app/preprocess/create_2_itemsets.py
@@ -0,0 +1,17 @@
+import sys
+import json
+sys.path.insert(0, '..')
+
+from models.data import session_factory as session
+
+
+for line in sys.stdin:
+	tag1, tag2 = map(int, line.strip().split())
+	query1 = "select postsid from poststags where tagsid = " + str(tag1)
+	query2 = "select postsid from poststags where tagsid = " + str(tag2)
+	query = "select count(*) from (" + query1 + " intersect " + query2 + ") t"
+
+	res = session.execute(query)
+	print tag1, tag2, list(res)[0][0]
+	sys.stdout.flush()
+
