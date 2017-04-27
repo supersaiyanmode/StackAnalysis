5d287fc0f6ee4092575fc73244ca3a586a1a9e83
Srivatsan Iyer <srriyer@indiana.edu>
2017-04-27 02:12:16 -0400

    Show user details skeleton.

--- app/static/views/user_details/main.js
+++ app/static/views/user_details/main.js
@@ -1,84 +1,92 @@
-function getUserDetails(successFn, errorFn) {
+function loadUserInfoDetails(userId, successFn, errorFn) {
 	$.ajax({
 		type: "GET",
-		url: "/questions/",
-		data:{author_id :123}
+		url: "/users/" + userId + "/"
 	}).done(successFn).fail(errorFn);
 }
 
-function loadUserQuestiondetails() {
-	var filterStr = [
-		["filter", JSON.stringify([{
-			"col": "author_id",
-			"op": "__eq__",
-			"val": "123"
-			}])],
-		["sort", JSON.stringify([{
-			"col": "score",
-			"order": "desc"
-		}])]
-	].map(function(item) {
-		return item[0] + "=" + encodeURIComponent(item[1]);
-	}).join("&");
-
-	var params = {
-		paginationSelector: 'div.container-fluid .pagination1-bar',
-		selector: 'div.container-fluid div.questions-content',
-		url: "/questions/?" +filterStr 
-	}
-	getUserDetails(
-		function success(obj) {
-			var tableObj = makeTable(params);
-			tableObj.load();
-		},
-		function fail(obj) {
-			$('#user-content').html("Unable to load data.");
-		}
-	);
-	
-
-
+function loadLocationInfoDetails(locId, successFn, errorFn) {
+	$.ajax({
+		type: "GET",
+		url: "/locations/" + locId + "/"
+	}).done(successFn).fail(errorFn);
 }
 
-function loadUserAnswerdetails() {
-	var filterStr = [
-		["filter", JSON.stringify([{
+function loadUserTagDetails(userId, successFn, errorFn) {
+	var url = "/users_tags/?filter=%5B%7B%22col%22%3A%22user_id" + 
+				"%22%2C%22op%22%3A%22__eq__%22%2C%22val%22%3A" + userId + 
+				"%7D%5D&sort=%5B%7B%22col%22%3A%22answer_count" +
+				"%22%2C%22order%22%3A%22desc%22%7D%5D&page=0";
+	$.ajax({
+		type: "GET",
+		url: url
+	}).done(successFn).fail(errorFn);
+}
+
+function loadUserInfo(data) {
+	$(".user-name").html(data.name);
+	$(".user-reputation").html(data.reputation);
+	$(".user-image").html('<img class="img-responsive page-header" src="/users/' + data.id + '/dp" />');
+	loadLocationInfoDetails(data.location_id, loadLocationInfo, null);
+}
+
+function loadLocationInfo(data) {
+	$(".user-location").html(data.location);
+	var location = [data.city, data.state, data.country].map(function(x) {
+			return x || "";
+		}).filter(function(x) { return !!x; }).join("");
+	$(".user-resolved-location").html(location);
+}
+
+function loadUserTags(userId) {
+	loadUserTagDetails(userId, function(data) {
+		//loading bubble.
+	}, function(data) { alert("Failed to load data."); });
+}
+
+function loadUserQuestiondetails(userId) {
+	var params = {
+		paginationSelector: 'div.container-fluid .questions-section .pagination-bar',
+		selector: "div.container-fluid .questions-section div.table-row",
+		rowCountSelector: '.questions-section .row-count-display',
+		url: "/questions/",
+		tableOrderData: [{"col": "score", "order": "desc"}],
+		tableFilterData: [{
 			"col": "author_id",
 			"op": "__eq__",
-			"val": "123"
-			}])],
-		["sort", JSON.stringify([{
-			"col": "score",
-			"order": "desc"
-		}])]
-	].map(function(item) {
-		return item[0] + "=" + encodeURIComponent(item[1]);
-	}).join("&");
+			"val": userId
+		}]
+	};
+	var tableObj = makeTable(params);
+	tableObj.load();
+}
 
+function loadUserAnswerdetails(userId) {
+	var selector = 'div.container-fluid .answers-section .table-content';
 	var params = {
-		paginationSelector: 'div.container-fluid .pagination2-bar',
-		selector: 'div.container-fluid div.answers-content',
-		url: "/answers/?" +filterStr 
-	}
-	getUserDetails(
-		function success(obj) {
-			var tableObj = makeTable(params);
-			tableObj.load();
-		},
-		function fail(obj) {
-			$('#user-content').html("Unable to load data.");
-		}
-	);
-	
-
-
+		paginationSelector: 'div.container-fluid .answers-section .pagination-bar',
+		selector: "div.container-fluid .answers-section div.table-row",
+		rowCountSelector: '.answers-section .row-count-display',
+		url: "/answers/",
+		tableOrderData: [{"col": "score", "order": "desc"}],
+		tableFilterData: [{
+			"col": "author_id",
+			"op": "__eq__",
+			"val": userId
+		}]
+	};
+	var tableObj = makeTable(params);
+	tableObj.load();
 }
 
 
 function onload() {
-	loadUserQuestiondetails();
-	loadUserAnswerdetails();
+	var userId = 227884;
+	loadUserQuestiondetails(userId);
+	loadUserAnswerdetails(userId);
+	loadUserInfoDetails(userId, loadUserInfo, function(data) {alert("Error..");});
+	loadUserTags(userId);
 }
 
 $(document).ready(onload);
-//# sourceURL=dashboard.js
+//# sourceURL=user_details.js
